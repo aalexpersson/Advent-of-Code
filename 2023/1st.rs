d4a0c80babf464601036dee32f28c04cb56fe8dc
aalexpersson <alex.persson0408@gmail.com>
2023-12-01 17:32:27 +0100

    reads input and finds digits of each line

--- 2023/1st.rs
+++ 2023/1st.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+    let mut value;
+
+    for line in reader.lines() {
+        //println!("{}", line?);
+        value = line.unwrap();
+        println!("{}", value);
+        find_digits(value);
+    }
+
+    Ok(())
+
+}
+
+fn find_digits(line: String) {
+    let values = line.chars();
+    for c in values {
+        if c.is_digit(10) {
+            println!("{}", c);
+        } 
+    }    
 }
