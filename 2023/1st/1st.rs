d55cb8262a42569503b99b164a28b3dd4eae4585
aalexpersson <alex.persson0408@gmail.com>
2023-12-02 16:12:56 +0100

    part 1 done, gives correct answer

--- 2023/1st/1st.rs
+++ 2023/1st/1st.rs
@@ -6,33 +6,31 @@ fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
     let mut value;
+    let mut sum = 0;
 
     for line in reader.lines() {
-        //println!("{}", line?);
         value = line.unwrap();
-        println!("{}", value);
-        find_digits(value);
+        sum += find_sum(value);
     }
 
-    Ok(())
+    println!("{}",sum);
 
+    Ok(())
+    
 }
 
-fn find_digits(line: String) {
+fn find_sum(line: String) -> i32{
     let values = line.chars();
     let mut numbers = Vec::new();
     let mut complete = String::new();
 
-    //let second_element = &list[1];
-
     for c in values {
         if c.is_digit(10) {
-            //println!("{}", c);
             let number = (c.to_string()).parse::<i32>().unwrap();
             numbers.push(number);
         } 
     }    
     complete.push_str(&numbers[0].to_string());
     complete.push_str(&numbers[numbers.len() - 1].to_string());
-    println!("{}", complete)
+    return complete.parse::<i32>().unwrap();
 }
