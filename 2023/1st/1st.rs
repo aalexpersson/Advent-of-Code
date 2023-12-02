5b5cd011af47a01873bc3228b716e37157edfe5d
aalexpersson <alex.persson0408@gmail.com>
2023-12-02 19:43:00 +0100

    [Issue] - gives almost correct answer for part 2

--- 2023/1st/1st.rs
+++ 2023/1st/1st.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-
+use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
@@ -10,7 +10,8 @@ fn main() -> io::Result<()> {
 
     for line in reader.lines() {
         value = line.unwrap();
-        sum += find_sum(value);
+        sum += find_sum(value.clone());
+        find_sum(value);
     }
 
     println!("{}",sum);
@@ -20,17 +21,52 @@ fn main() -> io::Result<()> {
 }
 
 fn find_sum(line: String) -> i32{
-    let values = line.chars();
+   // let mut values = line.chars();
     let mut numbers = Vec::new();
+    let mut substring_vec = Vec::new();
+    let mut replacement;
     let mut complete = String::new();
 
+    let word_digit = HashMap::from([
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ]);
+   
+    for i in 0..line.len() {
+        for j in i..line.len() {
+            let substring = &line[i..=j];
+
+            // Check if the substring contains a key in the hashmap
+            if word_digit.contains_key(substring) {
+                substring_vec.push(substring);
+               //println!("{:?}", substring_vec);
+            }
+        }
+    }
+
+    let mut new_line = line.clone();
+    for entry in substring_vec{
+        replacement = new_line.replace(entry, &word_digit.get(entry).unwrap().to_string());
+        new_line = replacement.clone();
+    }
+    let values = new_line.chars();
     for c in values {
         if c.is_digit(10) {
             let number = (c.to_string()).parse::<i32>().unwrap();
             numbers.push(number);
+            //println!("{}", number);
         } 
-    }    
+    }   
+    //print!("{:?}", numbers);
     complete.push_str(&numbers[0].to_string());
     complete.push_str(&numbers[numbers.len() - 1].to_string());
+    println!("{}", complete);
     return complete.parse::<i32>().unwrap();
 }
