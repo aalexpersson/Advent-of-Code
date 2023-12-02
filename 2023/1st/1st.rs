c154a5037e57a7885cc6726f93b409afa06d1e1f
aalexpersson <alex.persson0408@gmail.com>
2023-12-02 15:59:58 +0100

    finds and concatenates the first and last number

--- 2023/1st/1st.rs
+++ 2023/1st/1st.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
+
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
@@ -19,9 +20,19 @@ fn main() -> io::Result<()> {
 
 fn find_digits(line: String) {
     let values = line.chars();
+    let mut numbers = Vec::new();
+    let mut complete = String::new();
+
+    //let second_element = &list[1];
+
     for c in values {
         if c.is_digit(10) {
-            println!("{}", c);
+            //println!("{}", c);
+            let number = (c.to_string()).parse::<i32>().unwrap();
+            numbers.push(number);
         } 
     }    
+    complete.push_str(&numbers[0].to_string());
+    complete.push_str(&numbers[numbers.len() - 1].to_string());
+    println!("{}", complete)
 }
