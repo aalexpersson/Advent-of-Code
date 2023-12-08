45faef3afce685b15293e8840708101b5c494f4e
Alexander Persson <alex.persson0408@gmail.com>
2023-12-08 02:24:09 +0100

    Started day 2, extracts all draws

--- 2023/2nd/2nd.rs
+++ 2023/2nd/2nd.rs
@@ -8,7 +8,7 @@ fn main() -> io::Result<()> {
     for line in reader.lines() {
         //println!("{:?}", line.unwrap());
         let value = line.unwrap();
-        find_sum(value);
+        count(value);
     }
 
     Ok(())
@@ -16,9 +16,18 @@ fn main() -> io::Result<()> {
 }
 
 fn count(line: String){
-    let maxRed = 12;
-    let maxGreen = 13;
-    let maxBlue = 14;
+    let max_red = 12;
+    let max_geen = 13;
+    let max_blue = 14;
 
-    
+    let game = line.split(":").next().unwrap();
+    let game_id = game.rsplit_once(" ").unwrap().1;
+
+    let game_draws = line.strip_prefix(&(game.to_owned() + ": ")).unwrap();
+    let draws = game_draws.split(";").collect::<Vec<&str>>();
+    for c in draws{
+        println!("{}", c) 
+    }
+
+    //println!("{:?}", draws);
 }
